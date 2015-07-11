4df1745a3bca3bab17ca3d3521bebb94138972e0
vic <vicxizhu@gmail.com>
2015-07-11 19:01:00 -0400

    Add basic css to app

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,7 +1,6 @@
-require 'pry'
-
 get '/users/:id' do
   user = current_user
+  @username = User.find_by(id: session[:user_id]).username
   if user.nil?
     redirect '/signup'
   elsif params[:id].to_i == session[:user_id]
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,7 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
   has_many :guesses
+
+  validates :name, presence: true
+
 end
